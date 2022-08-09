090c5c7a5b8189a28427d3b2a8bfa09dc5e05445
misheck12 <misheck1720@gmail.com>
2022-08-09 14:28:43 +0200

    create model migrations

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :update_allowed_perimeters, if:
+    :devise_controller?
+
+    protected
+
+    def update_allowed_perimeters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password)}
+    end
+end
