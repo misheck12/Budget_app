f953d41efdefc1e5dee1401aa52e29c62005e7fc
misheck12 <misheck1720@gmail.com>
2022-08-11 12:49:37 +0200

    create home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    before_action :update_allowed_perimeters, if:
-    :devise_controller?
+  protected
 
-    protected
-
-    def update_allowed_perimeters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password)}
-    end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
